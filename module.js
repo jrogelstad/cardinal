a47cc8f296bc1fc0f8a540f2cd035f4ad94d3c22
John Rogelstad <john@rogelstad.net>
2016-01-02 07:30:48 -0500

    Expand definitions

--- module.js
+++ module.js
@@ -1,34 +1,78 @@
-/**
-    Framework for building object relational database apps
-
-    Copyright (C) 2015  John Rogelstad
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**/
-
-/*global m, f, window */
-
+/*global f */
 (function () {
-
   "strict";
 
   var relations = [
       {
         feather: "Role",
         valueProperty: "name",
-        labelProperty: "description"
+        labelProperty: "description",
+        form: {
+          name: "Role",
+          attrs: [{
+              attr: "name"
+            }, {
+              attr: "description"
+            }, {
+              attr: "createdBy"
+            }, {
+              attr: "updated"
+            }, {
+              attr: "updatedBy"
+            }
+          ]
+        },
+        list: {
+          columns: [{
+              attr: "name"
+            }, {
+              attr: "description"
+            }, {
+              attr: "createdBy"
+            }, {
+              attr: "updated"
+            }, {
+              attr: "updatedBy"
+            }
+          ]
+        }
       }, {
         feather: "Opportunity",
         valueProperty: "number",
-        labelProperty: "name"
+        labelProperty: "name",
+        form: {
+          name: "Opportunity",
+          attrs: [{
+              attr: "number"
+            }, {
+              attr: "name"
+            }, {
+              attr: "description"
+            }, {
+              attr: "createdBy"
+            }, {
+              attr: "updated"
+            }, {
+              attr: "updatedBy"
+            }
+          ]
+        },
+        list: {
+          columns: [{
+              attr: "number"
+            }, {
+              attr: "name"
+            }, {
+              attr: "description"
+            }, {
+              attr: "createdBy"
+            }, {
+              attr: "updated"
+            }, {
+              attr: "updatedBy"
+            }
+          ]
+        }
       }
     ];
 
