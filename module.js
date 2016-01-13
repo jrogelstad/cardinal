f16882b70fb66ce4c16a882d42f9455bcf571182
John Rogelstad <john@rogelstad.net>
2016-01-12 20:04:47 -0500

    Switch to data driven relation widgets

--- module.js
+++ module.js
@@ -2,50 +2,6 @@
 (function () {
   "strict";
 
-  var relations = [
-      {
-        feather: "Role",
-        valueProperty: "name",
-        labelProperty: "description",
-        form: "dbflv6oz3d10",
-        list: {
-          columns: [{
-              attr: "name"
-            }, {
-              attr: "description"
-            }, {
-              attr: "createdBy"
-            }, {
-              attr: "updated"
-            }, {
-              attr: "updatedBy"
-            }
-          ]
-        }
-      }, {
-        feather: "Opportunity",
-        valueProperty: "number",
-        labelProperty: "name",
-        form: "syva8q1zh98d",
-        list: {
-          columns: [{
-              attr: "number"
-            }, {
-              attr: "name"
-            }, {
-              attr: "description"
-            }, {
-              attr: "createdBy"
-            }, {
-              attr: "updated"
-            }, {
-              attr: "updatedBy"
-            }
-          ]
-        }
-      }
-    ];
-
-  relations.forEach(f.buildRelationWidget);
+  // Placeholder for future functionality
 
 }());
