4f0bdaa3e0998feac74c64fc1bb2d2f997adb6cc
John Rogelstad <john@rogelstad.net>
2016-03-01 19:20:40 -0500

    Separate controllers from routes

---
+++ controller-inventory.js
@@ -0,0 +1,245 @@
+/*global datasource*/
+(function (datasource) {
+  "strict";
+
+  var f = require("./common/core"),
+    math = require("mathjs"),
+    jsonpatch = require("fast-json-patch");
+
+  // Register database procedure on datasource
+  var adjust = function (obj) {
+    var afterCreditLocationBalance, afterDebitLocationBalance,
+      afterItem, afterDebitLocation, afterCreditLocation, 
+      creditLocationBalance, debitLocationBalance, transaction,
+      postRequest, postTransaction, afterPostTransaction, afterPostBalance,
+      requestId,
+      COMPLETE = 5,
+      POSTED = 3,
+      data = obj.specs,
+      item = data.item,
+      debitLocation = data.location,
+      creditLocation = {id: "u69aj7cgtkmf"},
+      date = data.date || f.now(),
+      note = data.note,
+      quantity = data.quantity,
+      n = 0;
+
+    afterItem = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      n += 1;
+      item = resp;
+      if (n === COMPLETE) { postRequest(); }
+    };
+
+    afterCreditLocation = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      n += 1;
+      creditLocation = resp;
+      if (n === COMPLETE) { postRequest(); }
+    };
+
+    afterDebitLocation = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      n += 1;
+      debitLocation = resp;
+      if (n === COMPLETE) { postRequest(); }
+    };
+
+    afterCreditLocationBalance = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      n += 1;
+      if (resp.length) {
+        creditLocationBalance = resp[0];
+        creditLocationBalance.balance = math.add(creditLocationBalance.balance, quantity);
+      } else {
+        creditLocationBalance = {
+          id: f.createId(),
+          node: item,
+          container: creditLocation,
+          balance: quantity
+        };
+      }
+      if (n === COMPLETE) { postRequest(); }
+    };
+
+    afterDebitLocationBalance = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      n += 1;
+      if (resp.length) {
+        debitLocationBalance = resp[0];
+        debitLocationBalance.balance = math.subtract(debitLocationBalance.balance, quantity);
+      } else {
+        debitLocationBalance = {
+          id: f.createId(),
+          node: item,
+          container: debitLocation,
+          balance: quantity * -1
+        };
+      }
+      if (n === COMPLETE) { postRequest(); }
+    };
+
+    postRequest = function () {
+      requestId = f.createId();
+      datasource.request({
+        method: "POST",
+        name: "Request",
+        client: obj.client,
+        callback: postTransaction,
+        isPosted: true,
+        data: {
+          id: requestId,
+          node: item,
+          date: date,
+          note: note,
+          distributions: [{
+            container: debitLocation,
+            quantity: quantity
+          }, {
+            container: creditLocation,
+            quantity: quantity * -1
+          }]
+        }       
+      }, true);
+    };
+
+    postTransaction = function (err) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+
+      n = 0;
+      transaction = {
+        node: item,
+        parent: {id: requestId},
+        date: date,
+        note: note,
+        distributions: [{
+          container: debitLocation,
+          quantity: quantity
+        }, {
+          container: creditLocation,
+          quantity: quantity * -1
+        }]
+      };
+
+      datasource.request({
+        method: "POST",
+        name: "Transaction",
+        client: obj.client,
+        callback: afterPostTransaction,
+        data: transaction      
+      }, true);
+
+      datasource.request({
+        method: "POST",
+        name: "LocationBalance",
+        id: debitLocationBalance.id,
+        client: obj.client,
+        callback: afterPostBalance,
+        data: debitLocationBalance
+      }, true);
+      
+      datasource.request({
+        method: "POST",
+        name: "LocationBalance",
+        id: creditLocationBalance.id,
+        client: obj.client,
+        callback: afterPostBalance,
+        data: creditLocationBalance
+      }, true);
+    };
+
+    afterPostBalance = function (err) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      n += 1;
+      if (n === POSTED) {
+        obj.callback(null, transaction);
+      }
+    };
+
+    afterPostTransaction = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      jsonpatch.apply(resp, transaction);
+      afterPostBalance();
+    };
+
+    // Real work starts here
+    datasource.request({
+      method: "GET",
+      name: "Item",
+      client: obj.client,
+      callback: afterItem,
+      id: item.id
+    }, true);
+
+    datasource.request({
+      method: "GET",
+      name: "Location",
+      client: obj.client,
+      callback: afterDebitLocation,
+      id: debitLocation.id
+    }, true);
+
+    datasource.request({
+      method: "GET",
+      name: "Location",
+      client: obj.client,
+      callback: afterCreditLocation,
+      id: creditLocation.id
+    }, true);
+
+    datasource.request({
+      method: "GET",
+      name: "LocationBalance",
+      client: obj.client,
+      callback: afterCreditLocationBalance,
+      filter: {
+        criteria: [{
+          property: "node",
+          value: item},{
+          property: "container",
+          value: creditLocation}]
+      }
+    }, true);
+
+    datasource.request({
+      method: "GET",
+      name: "LocationBalance",
+      client: obj.client,
+      callback: afterDebitLocationBalance,
+      filter: {
+        criteria: [{
+          property: "node",
+          value: item},{
+          property: "container",
+          value: debitLocation}]
+      }
+    }, true);
+  };
+
+  datasource.registerFunction("POST", "adjust", adjust);
+
+}(datasource));
---
+++ controller-ledger.js
@@ -0,0 +1,228 @@
+/*global datasource*/
+(function (datasource) {
+  "strict";
+
+  var f = require("./common/core"),
+    math = require("mathjs"),
+    jsonpatch = require("fast-json-patch");
+
+  // Register database procedure on datasource
+  var journalEntry = function (obj) {
+    var afterAccount, afterCurrency, afterLedgerBalance,
+      createJournal, postJournal,
+      afterPostBalance, afterPostTransaction,
+      count, request, transaction, raiseError,
+      data = obj.specs,
+      n = 0;
+
+    if (!Array.isArray(data.distributions)) {
+      obj.callback("Distributions must be a valid array.");
+      return;
+    }
+    count = data.distributions.length * 2 + 1;
+
+    afterCurrency = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      if (!resp) {
+        obj.callback("Invalid currency \"" + data.currency.id + "\".");
+        return;
+      }
+      n += 1;
+      if (n === count) { createJournal(); }
+    };
+
+    afterAccount = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+      if (!resp) {
+        obj.callback("Invalid ledger account \"" + this.ledgerAccount.id + "\".");
+        return;
+      }
+      n += 1;
+      if (n === count) { createJournal(); }
+    };
+
+    afterLedgerBalance = function (err, resp) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+
+      n += 1;
+      if (resp.length) {
+        this.ledgerBalance = resp[0];
+      } else {
+        this.ledgerBalance = {
+          id: f.createId(),
+          node: data.currency,
+          container: this.ledgerAccount,
+          balance: 0
+        };
+      }
+      if (n === count) { createJournal(); }
+    };
+
+    createJournal = function () {
+      var sumcheck = 0,
+        quantity = 0;
+
+      // Create baseline request
+      request = {
+        id: f.createId(),
+        node: data.currency,
+        date: data.time || f.today(),
+        note: data.note,
+        reference: data.reference,
+        isPosted: true,
+        distributions: []
+      };
+
+      // Build request distributions
+      data.distributions.forEach(function (dist) {
+        if (dist.debit) {
+          if (dist.debit <= 0) {
+            obj.callback("Debit must be a positive number.");
+            return;
+          }
+          sumcheck = math.subtract(sumcheck, dist.debit);
+          quantity = dist.debit * -1;
+        }
+
+        if (dist.credit) {
+          if (dist.credit <= 0) {
+            obj.callback("Credit must be a positive number.");
+            return;
+          }
+          sumcheck = math.add(sumcheck, dist.credit);
+          quantity = dist.credit;
+        }
+
+        dist.ledgerBalance.balance = math.add(dist.ledgerBalance.balance, quantity);
+
+        request.distributions.push({
+          container: dist.LedgerAccount,
+          quantity: quantity
+        });
+      });
+
+      // Check balance
+      if (sumcheck !== 0) {
+        obj.callback("Distribution does not balance.");
+        return;
+      }
+
+      // Create journal
+      datasource.request({
+        method: "POST",
+        name: "GeneralJournal",
+        client: obj.client,
+        callback: postJournal,
+        data: request     
+      }, true);
+    };
+
+    postJournal = function (err) {
+      if (err) {
+        obj.callback(err);
+        return;
+      }
+
+      n = 0;
+      count = request.distributions.length + 1;
+      transaction = {
+        node: data.currency,
+        parent: request,
+        date: data.date || f.today(),
+        note: data.note,
+        distributions: request.distributions
+      };
+
+      // Post journal
+      datasource.request({
+        method: "POST",
+        name: "GeneralLedger",
+        client: obj.client,
+        callback: afterPostTransaction,
+        data: transaction      
+      }, true);
+  
+      // Post balance updates
+      data.distributions.forEach(function (dist) {
+        datasource.request({
+          method: "POST",
+          name: "LedgerBalance",
+          id: dist.ledgerBalance.id,
+          client: obj.client,
+          callback: afterPostBalance,
+          data: dist.ledgerBalance
+        }, true);
+      });
+    };
+
+    afterPostBalance = function (err) {
+      if (err && !raiseError) {
+        raiseError = err;
+      }
+      n += 1;
+      if (n === count) {
+        // Raise error only after to all requests
+        // completed to ensure complete rollback
+        if (raiseError) {
+          obj.callback(raiseError);
+          return;
+        }
+        obj.callback(null, transaction);
+      }
+    };
+
+    afterPostTransaction = function (err, resp) {
+      if (err) {
+        afterPostBalance(err);
+        return;
+      }
+      jsonpatch.apply(resp, transaction);
+      afterPostBalance();
+    };
+
+    // Real work starts here
+    data.distributions.forEach(function (dist) {
+      datasource.request({
+        method: "GET",
+        name: "LedgerAccount",
+        client: obj.client,
+        callback: afterAccount.bind(dist),
+        id: dist.ledgerAccount.id
+      }, true);
+
+      datasource.request({
+        method: "GET",
+        name: "LedgerBalance",
+        client: obj.client,
+        callback: afterLedgerBalance.bind(dist),
+        filter: {
+          criteria: [{
+            property: "node",
+            value: data.currency},{
+            property: "container",
+            value: dist.ledgerAccount}]
+        }
+      }, true);
+     });
+
+    datasource.request({
+      method: "GET",
+      name: "Currency",
+      client: obj.client,
+      callback: afterCurrency,
+      id: data.currency.id
+    }, true);
+  };
+
+  datasource.registerFunction("POST", "journalEntry", journalEntry);
+
+}(datasource));
