8ba266df82676732a6c0fee0debff1a30203a6e0
John Rogelstad <john@rogelstad.net>
2016-03-06 16:13:52 -0500

    Auto populate trial balance

---
+++ ledger/controller-fiscal-period.js
@@ -0,0 +1,198 @@
+/*global datasource*/
+(function (datasource) {
+  "strict";
+
+  var f = require("./common/core"),
+    jsonpatch = require("fast-json-patch");
+
+  var doUpsertFiscalPeriod = function (obj) {
+    var afterPrevPeriod, afterUpsertPeriod,
+      afterFiscalPeriod, afterCurrency, afterLedgerAccount,
+      account, prevPeriod, createTrialBalance, done,
+      raiseError, accounts, currency, result,
+      client = obj.client,
+      callback = obj.callback,
+      fiscalPeriod = obj,
+      id = fiscalPeriod.id || f.createId(),
+      n = 0,
+      count = 4,
+      found = false;
+
+    delete fiscalPeriod.client;
+    delete fiscalPeriod.callback;
+
+    afterFiscalPeriod = function (err, resp) {
+      n += 1;
+      if (err) {
+        done(err);
+        return;
+      }
+
+      count += 1;
+      if (resp) { found = true; }
+
+      datasource.request({
+        method: "POST",
+        name: "doUpsert",
+        id: id,
+        data: {
+          name: "FiscalPeriod",
+          data: fiscalPeriod
+        },
+        client: client,
+        callback: afterUpsertPeriod
+      }, true);
+    };
+
+    afterPrevPeriod = function (err, resp) {
+      n += 1;
+      if (err) {
+        done(err);
+        return;
+      }
+
+      if (resp.length) {
+        prevPeriod = resp[0];
+      }
+      if (n === count) { createTrialBalance(); }
+    };
+
+    afterCurrency = function (err, resp) {
+      n += 1;
+      if (err) {
+        done(err);
+        return;
+      }
+      if (!resp.length) {
+        done("No base currency found");
+      }
+      currency = resp[0];
+      if (n === count) { createTrialBalance(); }
+    };
+
+    afterLedgerAccount = function (err, resp) {
+      n += 1;
+      if (err) {
+        done(err);
+        return;
+      }
+
+      accounts = resp;
+      if (n === count) { createTrialBalance(); }
+    };
+
+    afterUpsertPeriod = function (err, resp) {
+      n += 1;
+      if (err) {
+        done(err);
+        return;
+      }
+
+      jsonpatch.apply(fiscalPeriod, resp);
+      result = resp;
+      if (n === count) { createTrialBalance(); }
+    };
+
+    createTrialBalance = function (err) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      if (found || !accounts.length) {
+        done();
+        return;
+      }
+
+      account = accounts.shift();
+
+      datasource.request({
+        method: "POST",
+        name: "TrialBalance",
+        client: client,
+        callback: createTrialBalance,
+        data: {
+          node: currency,
+          container: account,
+          period: fiscalPeriod,
+          previous: prevPeriod
+        }     
+      }, true);
+    };
+
+    done = function (err) {
+      if (err && !raiseError) {
+        raiseError = err;
+        return;
+      }
+      if (n === count) {
+        if (raiseError) {
+          callback(raiseError);
+          return;
+        }
+        callback(null, result);
+      }
+    };
+
+    // Real work starts here
+    if (!fiscalPeriod.id) {
+      datasource.request({
+        method: "POST",
+        name: "doInsert",
+        data: {
+          name: "FiscalPeriod",
+          data: fiscalPeriod
+        },
+        client: client,
+        callback: afterUpsertPeriod
+      }, true);
+    } else {
+      datasource.request({
+        method: "GET",
+        name: "FiscalPeriod",
+        client: client,
+        callback: afterFiscalPeriod,
+        id: id
+      }, true);
+    }
+
+    datasource.request({
+      method: "GET",
+      name: "Currency",
+      client: client,
+      callback: afterCurrency,
+      filter: {
+        criteria: [{
+          property: "isBase",
+          value: true
+        }]
+      }
+    }, true);
+
+    datasource.request({
+      method: "GET",
+      name: "FiscalPeriod",
+      client: client,
+      callback: afterPrevPeriod,
+      filter: {
+        order: [{
+          property: "end",
+          operator: "<",
+          value: fiscalPeriod.end,
+          order: "DESC"
+        }],
+        limit: 1
+      }
+    }, true);
+
+    datasource.request({
+      method: "GET",
+      name: "LedgerAccount",
+      client: client,
+      callback: afterLedgerAccount
+    }, true);
+  };
+
+  datasource.registerFunction("POST", "FiscalPeriod", doUpsertFiscalPeriod);
+
+}(datasource));
