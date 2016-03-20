1e48f8c361ba48ed051a819a8a24072f3d879c4f
John Rogelstad <john@rogelstad.net>
2016-03-20 12:51:57 -0400

    Use new helper function

--- ledger/route-ledger.js
+++ ledger/route-ledger.js
@@ -5,33 +5,7 @@
   var express = require("express");
 
   // Register route to the public
-  var doRequest,
     router = express.Router();
-
-  doRequest = function (req, res) {
-   var payload, callback,
-      args = req.body;
-
-    callback = function (err, resp) {
-      if (err) {
-        res.status(err.statusCode).json(err.message);
-        return;
-      }
-
-      res.json(resp);
-    };
-
-    payload = {
-      method: "POST",
-      name: this,
-      user: "postgres", //getCurrentUser(),
-      callback: callback,
-      data: args
-    };
-
-    console.log(JSON.stringify(payload, null, 2));
-    datasource.request(payload);
-  };
 
   router.route("/post-journal").post(doRequest.bind("postJournal"));
   router.route("/post-journals").post(doRequest.bind("postJournals"));
