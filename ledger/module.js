e89d063abdb715d92acafdd9da4c3b64eb44f116
John Rogelstad <john@rogelstad.net>
2017-11-22 14:59:08 -0500

    Default transactions, settings

--- ledger/module.js
+++ ledger/module.js
@@ -1,5 +1,51 @@
-/*global f */
-(function () {
-  "strict";
+/**
+    Framework for building object relational database apps
+    Copyright (C) 2016  John Rogelstad
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+(function () {
+  "use strict";
+
+  console.log("Loading ledger module");
+
+  var catalog = require("catalog"),
+    model = require("model"),
+    list = require("list"),
+    models = catalog.register("models"),
+    feather = catalog.getFeather("GeneralJournal"),
+    settings = catalog.settings(),
+    ledgerSettings;
+
+  // Fetch ledger settings
+  ledgerSettings = settings("ledger");
+  ledgerSettings.fetch();
+
+  // Create general journal model
+  models.generalJournal = function (data) {
+    data = data || {};
+
+    // Set default currency on 'kind' attribute
+    var defaultCurrency = function () {
+      return ledgerSettings.data().defaultCurrency;
+    };
+    feather.properties.kind.default = defaultCurrency;
+
+    // Return instantiated model
+  	return model(data, feather);
+	};
+
+  models.generalJournal.list = list("GeneralJournal");
 }());
