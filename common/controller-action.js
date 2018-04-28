21fb73b24fb856e6450ddef84cb7e98da8b927f4
John Rogelstad <jrogelstad@users.noreply.github.com>
2018-04-28 15:10:41 -0400

    Clear is used
    Clear is used when transaction deleted.

--- common/controller-action.js
+++ common/controller-action.js
@@ -2,7 +2,35 @@
 (function (datasource) {
   "strict";
 
-  var doAfterInsertAction = function (obj) {
+  function getNodes (action) {
+    var that = this;
+    return new Promise (function (resolve, reject) {
+      var payload, ids;
+
+      ids = action.distributions.map(function (dist) {
+        return dist.node.id;
+      });
+
+      payload = {
+        method: "GET",
+        name: "Node",
+        client: that.client,
+        filter: {
+          criteria: [{
+            property: "id",
+            operator: "IN",
+            value: ids
+          }]
+        }
+      };
+
+      datasource.request(payload, true)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  function doAfterInsertAction (obj) {
     return new Promise (function (resolve, reject) {
       function updateNodes (nodes) {
         return new Promise (function (resolve, reject) {
@@ -26,25 +54,26 @@
         });
       }
 
-      function getNodes () {
+      Promise.resolve()
+        .then(getNodes.bind(obj, obj.data))
+        .then(updateNodes)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  datasource.registerFunction("POST", "Action",
+    doAfterInsertAction, datasource.TRIGGER_AFTER);
+
+  function doBeforeDeleteAction (obj) {
+    return new Promise (function (resolve, reject) {
+      function getAction () {
         return new Promise (function (resolve, reject) {
-          var payload, ids;
-
-          ids = obj.data.distributions.map(function (dist) {
-            return dist.node.id;
-          });
-
-          payload = {
+          var payload = {
             method: "GET",
-            name: "Node",
-            client: obj.client,
-            filter: {
-              criteria: [{
-                property: "id",
-                operator: "IN",
-                value: ids
-              }]
-            }
+            name: "Action",
+            id: obj.id,
+            client: obj.client
           };
 
           datasource.request(payload, true)
@@ -53,15 +82,89 @@
         });
       }
 
+      function cacheAction (action) {
+        return new Promise (function (resolve) {
+          obj.cache = action;
+          resolve();
+        });
+      }
+
       Promise.resolve()
-        .then(getNodes)
+        .then(getAction)
+        .then(cacheAction)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  datasource.registerFunction("DELETE", "Action",
+    doBeforeDeleteAction, datasource.TRIGGER_BEFORE);
+
+  function doAfterDeleteAction (obj) {
+    return new Promise (function (resolve, reject) {
+      var action = obj.cache;
+
+      function updateNodes (nodes) {
+        return new Promise (function (resolve, reject) {
+          var requests = nodes.map(function (node) {
+            var payload = {
+                method: "GET",
+                name: "Distribution",
+                properties: ["id"],
+                client: obj.client,
+                filter: {
+                  criteria: [{
+                    property: "node.id",
+                    operator: "=",
+                    value: node.id
+                  }],
+                  limit: 1
+                }
+              };
+
+            function callback (resp) {
+              return new Promise (function (resolve, reject) {
+                var payload2;
+
+                if (!resp.length) {
+                  node.isUsed = false;
+                  payload2 = {
+                    method: "POST",
+                    name: "Node",
+                    id: node.id,
+                    client: obj.client,
+                    data: node
+                  };
+
+                  datasource.request(payload2, true)
+                    .then(resolve)
+                    .catch(reject);
+                  return;
+                }
+
+                resolve();
+              });        
+            }
+
+            return datasource.request(payload, true)
+              .then(callback);
+          });
+
+          Promise.all(requests)
+            .then(resolve)
+            .catch(reject);
+        });
+      }
+
+      Promise.resolve()
+        .then(getNodes.bind(obj, action))
         .then(updateNodes)
         .then(resolve)
         .catch(reject);
     });
-  };
+  }
 
-  datasource.registerFunction("POST", "Action",
-    doAfterInsertAction, datasource.TRIGGER_AFTER);
+  datasource.registerFunction("DELETE", "Action",
+    doAfterDeleteAction, datasource.TRIGGER_AFTER);
 
 }(datasource));
