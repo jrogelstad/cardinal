73df8e8a06fbc68eab67812671e1a37c5183e2f6
John Rogelstad <john@rogelstad.net>
2019-01-20 10:32:36 -0500

    App Editing
    Prepare for in-app editing.

--- ledger/client/module.js
+++ ledger/client/module.js
@@ -15,9 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-/*jslint browser, this*/
-/*global f*/
-
+/*jslint this*/
 const catalog = f.catalog();
 const store = catalog.store();
 const model = store.factories().model;
--- sales/client/module.js
+++ sales/client/module.js
@@ -15,9 +15,6 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-/*jslint browser, this*/
-/*global f*/
-
 const catalog = f.catalog();
 const store = catalog.store();
 const model = store.factories().model;
