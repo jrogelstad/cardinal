88d8be7bdfab0f580c9ee22afabd6c44e56d80ba
John Rogelstad <john@rogelstad.net>
2019-02-10 18:57:51 -0500

    Follow up
    Demonstrate calculated icon.

--- crm/client/module.js
+++ crm/client/module.js
@@ -1,1 +1,73 @@
-// Create additional client models and components here
+const catalog = f.catalog();
+const model = catalog.store().factories().model;
+
+/**
+    Lead model
+*/
+function lead(data, feather) {
+    "use strict";
+
+    feather = feather || catalog.getFeather("Lead");
+    let that = model(data, feather);
+
+    function followUpIcon() {
+        if (that.data.followUp()) {
+            return "flag";
+        }
+
+        return "";
+    }
+
+    that.addCalculated({
+        name: "followUpIcon",
+        description: "Flagged for follow up",
+        type: "string",
+        format: "icon",
+        function: followUpIcon
+    });
+
+    return that;
+}
+
+lead.static = f.prop({
+    flagFollowUp: function (viewModel) {
+        "use strict";
+
+        viewModel.tableWidget().selections().forEach(
+            function (model) {
+                debugger
+                model.data.followUp(true);
+                model.save();
+            }
+        );
+    },
+    flagFollowUpCheck: function (selections) {
+        "use strict";
+
+        return selections.some((model) => !model.data.followUp());
+    },
+    unflagFollowUp: function (viewModel) {
+        "use strict";
+
+        viewModel.tableWidget().selections().forEach(
+            function (model) {
+                model.data.followUp(false);
+                model.save();
+            }
+        );
+    },
+    unflagFollowUpCheck: function (selections) {
+        "use strict";
+
+        return selections.some((model) => model.data.followUp());
+    }
+});
+
+lead.calculated = f.prop({
+    followUpIcon: {
+        type: "string",
+        format: "icon"
+    }
+});
+
+catalog.registerModel("Lead", lead, true);
